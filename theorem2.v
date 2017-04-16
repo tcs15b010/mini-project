a22962ff31d45a3e4450e7c2140c6be33a75177f
Deepikakadela <tcs15b011@iittp.ac.in>
2017-04-16 10:46:23 +0530

    Create theorem2.v

---
+++ theorem2.v
@@ -0,0 +1,24 @@
+(* Proving Propositional Logic in Coq*)
+
+Section PropositionalLogic.
+Variables P Q R : Prop.
+Theorem distr and or : (P ∧ Q) ∨ (P ∧ R) → P ∧ (Q ∨ R).
+intros H.
+destruct H as [H1 | H2 ].
+split.
+destruct H1 as [HP HQ].
+exact HP.
+destruct H1 as [HP HQ].
+left.
+exact HQ.
+destruct H2 as [HP HR].
+auto.
+Qed.
+Theorem noncontradiction : not (P ∧ not P).
+unfold not.
+intros H.
+destruct H as [H1 H2 ].
+auto.
+Qed.
+
+End PropositionalLogic.
